8f3bc89c13be47c04415df838c8a6e12220b3d9d
Andrew Chan <andrewkchan@berkeley.edu>
2017-05-22 19:45:48 -0700

    Refactored player into presentational/container components

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,288 +1,52 @@
 import React, { Component, PropTypes } from "react";
-import ReactDOM from "react-dom";
 
 import SeekBar from "../components/SeekBar";
 import PlayerSongInfo from "../components/PlayerSongInfo";
 
-import { changeCurrentPercent, changeSong, setIsPlaying, setIsSeeking, toggleIsPlaying } from "../actions/PlayerActions";
-import { formatSeconds, formatStreamUrl } from "../utils/FormatUtils";
-
-import { CHANGE_TYPES } from "../constants/SongConstants";
-
 const propTypes = {
     dispatch: PropTypes.func.isRequired,
-    player: PropTypes.object.isRequired,
-    playingSongId: PropTypes.number.isRequired,
-    playlists: PropTypes.object.isRequired,
-    song: PropTypes.object,
-    songs: PropTypes.object.isRequired,
-    users: PropTypes.object.isRequired
+    muted: PropTypes.bool.isRequired,           //whether volume is muted or not
+    nextFunc: PropTypes.func.isRequired,        //callback that skips to the next song
+    onSeekTimeFinishedFunc: PropTypes.func.isRequired,  //callback to call when done seeking through the song
+    onSeekVolumeFinishedFunc: PropTypes.func.isRequired,    //ballback to call when done seeking through volume bar
+    onSeekTimeFunc: PropTypes.func.isRequired,      //callback to call when seeking through the song
+    onSeekVolumeFunc: PropTypes.func.isRequired,    //callback to call when seeking through volume bar
+    player: PropTypes.object.isRequired,            //object containing global player state
+    prevFunc: PropTypes.func.isRequired,        //callback that skips to the previous song
+    repeat: PropTypes.bool.isRequired,          //whether "repeat" mode is active or not
+    song: PropTypes.object.isRequired,          //the song currently being played
+    shuffle: PropTypes.bool.isRequired,         //whether "shuffle" mode is active or not
+    toggleMuteFunc: PropTypes.func.isRequired,          //callback that toggles audio volume
+    togglePlayFunc: PropTypes.func.isRequired,      //callback that toggles audio playback
+    toggleRepeatFunc: PropTypes.func.isRequired,        //callback that toggles repeat mode
+    toggleShuffleFunc: PropTypes.func.isRequired,   //callback that toggles shuffle mode
+    user: PropTypes.object.isRequired,              //user object rep. author of the song
+    volume: PropTypes.number.isRequired,            //(0.0 to 1.0) volume level
 };
 
-const VOLUME_SCALE = 0.6; //full volume hurts my ears...
-
-/*
-Player Component
-
-The bar at the bottom of the app containing audio player controls. Also encapsulates the actual audio playing HTML element.
-*/
-
 class Player extends Component {
-    constructor(props) {
-        super(props);
-
-        const { song } = props;
-        const previousVolumeLevel = 1; //Number.parseFloat(LocalStorageUtils.get("volume"));
-        /*
-        State of the audio playback for the current song.
-        Note that song changes should ALWAYS update props rather than state.
-        */
-        this.state = {
-            muted: false,
-            repeat: false,
-            shuffle: false,
-            volume: previousVolumeLevel || 1
-        };
-        this._audio = new Audio(formatStreamUrl(song.stream_url));
-
-        this.handleEnded = this.handleEnded.bind(this);
-        this.handleLoadedMetadata = this.handleLoadedMetadata.bind(this);
-        this.handleLoadStart = this.handleLoadStart.bind(this);
-        this.handlePause = this.handlePause.bind(this);
-        this.handlePlay = this.handlePlay.bind(this);
-        this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
-        this.handleVolumeChange = this.handleVolumeChange.bind(this);
-        this.onSeekVolume = this.onSeekVolume.bind(this);
-        this.onSeekTime = this.onSeekTime.bind(this);
-        this.seekTimeFinished = this.seekTimeFinished.bind(this);
-        this.seekVolumeFinished = this.seekVolumeFinished.bind(this);
-        this.toggleMute = this.toggleMute.bind(this);
-        this.togglePlay = this.togglePlay.bind(this);
-        this.toggleRepeat = this.toggleRepeat.bind(this);
-        this.toggleShuffle = this.toggleShuffle.bind(this);
-    }
-
-    componentDidMount() {
-        this._audio.addEventListener("ended", this.handleEnded, false);
-        this._audio.addEventListener("loadedmetadata", this.handleLoadedMetadata, false);
-        this._audio.addEventListener("loadstart", this.handleLoadStart, false);
-        this._audio.addEventListener("pause", this.handlePause, false);
-        this._audio.addEventListener("play", this.handlePlay, false);
-        this._audio.addEventListener("timeupdate", this.handleTimeUpdate, false);
-        this._audio.addEventListener("volumechange", this.handleVolumeChange, false);
-        this._audio.volume = this.state.volume * VOLUME_SCALE;
-        const { player } = this.props;
-        if (player.isPlaying) {
-            this._audio.play();
-        }
-    }
-
-    componentDidUpdate(prevProps) {
-        const { dispatch, song, player } = this.props;
-        //handle any time updates that may have occurred
-        let time = Math.floor(player.percent * (song.duration / 1000.0));
-        if (prevProps.playingSongId !== this.props.playingSongId) {
-            this._audio.src = formatStreamUrl(song.stream_url);
-            if (player.outOfSync && !player.isSeeking) {
-                this._audio.currentTime = time;
-                dispatch(changeCurrentPercent(player.percent, false));
-            }
-        } else if (player.outOfSync) {
-            this._audio.currentTime = time;
-            dispatch(changeCurrentPercent(player.percent, false)); //turn off outOfSync flag
-        }
-        //handle playing and pausing updates
-        if (!player.isPlaying) {
-            this._audio.pause();
-        } else {
-            this._audio.play();
-        }
-    }
-
-    componentWillUnmount() {
-        this._audio.removeEventListener("ended", this.handleEnded, false);
-        this._audio.removeEventListener("loadedmetadata", this.handleLoadedMetadata, false);
-        this._audio.removeEventListener("loadstart", this.handleLoadStart, false);
-        this._audio.removeEventListener("pause", this.handlePause, false);
-        this._audio.removeEventListener("play", this.handlePlay, false);
-        this._audio.removeEventListener("timeupdate", this.handleTimeUpdate, false);
-        this._audio.removeEventListener("volumechange", this.handleVolumeChange, false);
-    }
-
-    changeSong(changeType) {
-        const { dispatch } = this.props;
-        dispatch(changeSong(changeType));
-    }
-
-    /*
-     * function handleEnded
-     * Handle the event fired when the current song ends.
-     */
-    handleEnded() {
-        if (this.state.repeat) {
-            this._audio.play();
-        } else if (this.state.shuffle) {
-            this.changeSong(CHANGE_TYPES.SHUFFLE);
-        } else {
-            this.changeSong(CHANGE_TYPES.NEXT);
-        }
-    }
-
-    /*
-     * function handleLoadedMetadata
-     * Handle when the audio player has successfully loaded song metadata.
-     */
-    handleLoadedMetadata() {
-        //empty
-    }
-
-    /*
-     * function handleLoadStart
-     * Handle when the player begins to load something.
-     */
-    handleLoadStart() {
-        const { dispatch } = this.props;
-        dispatch(changeCurrentPercent(0.0));
-    }
-
-    handlePause() {
-        // const { dispatch } = this.props;
-        // dispatch(setIsPlaying(false));
-    }
-
-    handlePlay() {
-        // const { dispatch } = this.props;
-        // dispatch(setIsPlaying(true));
-    }
-
-    /*
-     * function handleTimeUpdate
-     * Handle a time update event from the HTMLAudioElement. Don't change time if currently out of sync with the desired time.
-     */
-    handleTimeUpdate(e) {
-        const { dispatch, player, song } = this.props;
-        const currentTime = Math.floor(this._audio.currentTime);
-        if (player.outOfSync) {
-            return;
-        }
-
-        dispatch(changeCurrentPercent(currentTime / (song.duration / 1000.0)));
-    }
-
-    /*
-     * function handleVolumeChange
-     * Handle a volume changed event from the HTMLAudioElement.
-     */
-    handleVolumeChange(e) {
-        const volume = this._audio.volume / VOLUME_SCALE;
-        //LocalStorageUtils.set("volume", volume);
-        this.setState({
-            volume
-        });
-    }
-
-    /*
-     * function onSeekTime
-     * Handle when the user moves the mouse pointer to seek through the track.
-     * @param   percent     A number between 0.0 and 1.0 indicating seek progress.
-     */
-    onSeekTime(percent) {
-        const { dispatch, player } = this.props;
-        if (!player.isSeeking) {
-            dispatch(setIsSeeking(true));
-        }
-        dispatch(changeCurrentPercent(percent, true));
-    }
-
-    /*
-     * function onSeekTime
-     * Handle when the user moves the mouse pointer to change the volume.
-     * @param   percent     A number between 0.0 and 1.0 indicating volume bar percent.
-     */
-    onSeekVolume(percent) {
-        this.setState({
-            volume: percent
-        });
-        this._audio.volume = percent * VOLUME_SCALE;
-    }
-
-    /*
-     * function seekTimeFinished
-     * Handle when the user finishes seeking through the track.
-     * @param   percent     A number between 0.0 and 1.0 indicating seek progress.
-     */
-    seekTimeFinished(percent) {
-        // const { player } = this.props;
-        const { dispatch } = this.props;
-        dispatch(setIsSeeking(false));
-    }
-
-    /*
-     * function seekVolumeFinished
-     * Handle when the user finishes changing volume.
-     * @param   percent     A number between 0.0 and 1.0 indicating volume percent.
-     */
-    seekVolumeFinished(percent) {
-        this.setState({
-            volume: percent
-        });
-        this._audio.volume = percent * VOLUME_SCALE;
-    }
-
-    toggleMute() {
-        this._audio.muted = !this._audio.muted;
-        this.setState({
-            muted: this._audio.muted
-        });
-    }
-
-    /*
-     * function togglePlay
-     * Toggle audio playback.
-     */
-    togglePlay() {
-        const { dispatch, player } = this.props;
-        const { isPlaying } = player;
-        if (isPlaying) {
-            //this._audio.pause();
-            dispatch(setIsPlaying(false));
-        } else {
-            //this._audio.play();
-            dispatch(setIsPlaying(true));
-        }
-    }
-
-    /*
-     * function toggleRepeat
-     * Toggle whether to repeat the current track after finishing playback.
-     */
-    toggleRepeat() {
-        this.setState({
-            repeat: !this.state.repeat
-        });
-    }
-
-    /*
-     * function toggleShuffle
-     * Toggle shuffle playback mode.
-     */
-    toggleShuffle() {
-        this.setState({
-            shuffle: !this.state.shuffle
-        });
-    }
-
     render() {
-        const { dispatch, player, playingSongId, songs, song, users } = this.props;
+        const {
+            dispatch,
+            muted,
+            nextFunc,
+            onSeekTimeFinishedFunc,
+            onSeekVolumeFinishedFunc,
+            onSeekTimeFunc,
+            onSeekVolumeFunc,
+            player,
+            prevFunc,
+            repeat,
+            song,
+            shuffle,
+            toggleMuteFunc,
+            togglePlayFunc,
+            toggleRepeatFunc,
+            toggleShuffleFunc,
+            user,
+            volume,
+        } = this.props;
         const { isPlaying, percent } = player;
-        const { muted, volume } = this.state;
-        const user = users[song.user_id];
-        const prevFunc = this.changeSong.bind(this, CHANGE_TYPES.PREV);
-        const nextFunc = this.changeSong.bind(
-            this,
-            this.state.shuffle ? CHANGE_TYPES.SHUFFLE : CHANGE_TYPES.NEXT
-        );
-
         let volumeIcon;
         if (muted || volume === 0) {
             volumeIcon = (<i className="icon ion-android-volume-mute" />);
@@ -315,8 +79,8 @@ class Player extends Component {
                         <div className="player-middle">
                             <div className="player-middle-section">
                                 <div
-                                    className={`player-button ${this.state.shuffle ? "active" : ""}`}
-                                    onClick={this.toggleShuffle}
+                                    className={`player-button ${shuffle ? "active" : ""}`}
+                                    onClick={toggleShuffleFunc}
                                 >
                                     <i className="icon ion-shuffle" />
                                 </div>
@@ -328,7 +92,7 @@ class Player extends Component {
                                 </div>
                                 <div
                                     className="player-button"
-                                    onClick={this.togglePlay}
+                                    onClick={togglePlayFunc}
                                 >
                                     <i className={isPlaying ? "icon ion-ios-pause" : "icon ion-ios-play"} />
                                 </div>
@@ -339,8 +103,8 @@ class Player extends Component {
                                     <i className="icon ion-ios-fastforward" />
                                 </div>
                                 <div
-                                    className={`player-button ${this.state.repeat ? "active" : ""}`}
-                                    onClick={this.toggleRepeat}
+                                    className={`player-button ${repeat ? "active" : ""}`}
+                                    onClick={toggleRepeatFunc}
                                 >
                                     <i className="icon ion-loop" />
                                 </div>
@@ -353,9 +117,9 @@ class Player extends Component {
                                         containerClassName="player-seek-container"
                                         initialProgress={percent}
                                         isVertical={false}
-                                        onSeek={this.onSeekTime}
+                                        onSeek={onSeekTimeFunc}
                                         progressClassName="player-seek-duration-bar"
-                                        seekFinished={this.seekTimeFinished}
+                                        seekFinished={onSeekTimeFinishedFunc}
                                         thumbClassName="player-seek-handle"
                                     />
                                 </div>
@@ -366,7 +130,7 @@ class Player extends Component {
                             {/*TODO: playlist popover menu*/}
                             <div
                                 className="player-button player-volume-button"
-                                onClick={this.toggleMute}
+                                onClick={toggleMuteFunc}
                             >
                                 {volumeIcon}
                             </div>
@@ -375,11 +139,11 @@ class Player extends Component {
                                     <SeekBar
                                         barClassName="player-seek-bar"
                                         containerClassName="player-seek-container"
-                                        initialProgress={this.state.volume}
+                                        initialProgress={volume}
                                         isVertical={false}
-                                        onSeek={this.onSeekVolume}
+                                        onSeek={onSeekVolumeFunc}
                                         progressClassName="player-seek-duration-bar"
-                                        seekFinished={this.seekVolumeFinished}
+                                        seekFinished={onSeekVolumeFinishedFunc}
                                         thumbClassName="player-seek-handle"
                                     />
                                 </div>
@@ -391,7 +155,3 @@ class Player extends Component {
         );
     }
 }
-
-Player.propTypes = propTypes;
-
-export default Player;
--- client/src/containers/PlayerContainer.js
+++ client/src/containers/PlayerContainer.js
@@ -1,26 +1,310 @@
 import React, { Component, PropTypes } from "react";
 import { connect } from "react-redux";
+
 import Player from "../components/Player";
 
+import { changeCurrentPercent, changeSong, setIsPlaying, setIsSeeking, toggleIsPlaying } from "../actions/PlayerActions";
+import { formatSeconds, formatStreamUrl } from "../utils/FormatUtils";
 import { getPlayingSongId } from "../utils/PlayerUtils";
 
+import { CHANGE_TYPES } from "../constants/SongConstants";
+
 const propTypes = {
-    isMobile: PropTypes.bool,
-    playingSongId: PropTypes.number
+    dispatch: PropTypes.func.isRequired,
+    player: PropTypes.object.isRequired,
+    playingSongId: PropTypes.number.isRequired,
+    playlists: PropTypes.object.isRequired,
+    song: PropTypes.object,
+    songs: PropTypes.object.isRequired,
+    users: PropTypes.object.isRequired
 };
 
+const VOLUME_SCALE = 0.6; //full volume hurts my ears...
+
+/*
+Player Container
+
+Encapsulates the all state related to audio playback, such as volume and the actual HTML audio element.
+Note we treat audio playback state as presentational state! What we store globally is audio playback *metadata*.
+*/
+
 class PlayerContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        const { song } = props;
+        const previousVolumeLevel = 1; //Number.parseFloat(LocalStorageUtils.get("volume"));
+        /*
+        State of the audio playback for the current song.
+        Note that song changes should ALWAYS update props rather than state.
+        */
+        this.state = {
+            muted: false,
+            repeat: false,
+            shuffle: false,
+            volume: previousVolumeLevel || 1
+        };
+        this._audio = new Audio(formatStreamUrl(song.stream_url));
+
+        this.handleEnded = this.handleEnded.bind(this);
+        this.handleLoadedMetadata = this.handleLoadedMetadata.bind(this);
+        this.handleLoadStart = this.handleLoadStart.bind(this);
+        this.handlePause = this.handlePause.bind(this);
+        this.handlePlay = this.handlePlay.bind(this);
+        this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
+        this.handleVolumeChange = this.handleVolumeChange.bind(this);
+        this.onSeekVolumeFunc = this.onSeekVolumeFunc.bind(this);
+        this.onSeekTimeFunc = this.onSeekTimeFunc.bind(this);
+        this.onSeekTimeFinishedFunc = this.onSeekTimeFinishedFunc.bind(this);
+        this.onSeekVolumeFinishedFunc = this.onSeekVolumeFinishedFunc.bind(this);
+        this.toggleMuteFunc = this.toggleMuteFunc.bind(this);
+        this.togglePlayFunc = this.togglePlayFunc.bind(this);
+        this.toggleRepeatFunc = this.toggleRepeatFunc.bind(this);
+        this.toggleShuffleFunc = this.toggleShuffleFunc.bind(this);
+    }
+
+    componentDidMount() {
+        this._audio.addEventListener("ended", this.handleEnded, false);
+        this._audio.addEventListener("loadedmetadata", this.handleLoadedMetadata, false);
+        this._audio.addEventListener("loadstart", this.handleLoadStart, false);
+        this._audio.addEventListener("pause", this.handlePause, false);
+        this._audio.addEventListener("play", this.handlePlay, false);
+        this._audio.addEventListener("timeupdate", this.handleTimeUpdate, false);
+        this._audio.addEventListener("volumechange", this.handleVolumeChange, false);
+        this._audio.volume = this.state.volume * VOLUME_SCALE;
+        const { player } = this.props;
+        if (player.isPlaying) {
+            this._audio.play();
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        const { dispatch, song, player } = this.props;
+        //handle any time updates that may have occurred
+        let time = Math.floor(player.percent * (song.duration / 1000.0));
+        if (prevProps.playingSongId !== this.props.playingSongId) {
+            this._audio.src = formatStreamUrl(song.stream_url);
+            if (player.outOfSync && !player.isSeeking) {
+                this._audio.currentTime = time;
+                dispatch(changeCurrentPercent(player.percent, false));
+            }
+        } else if (player.outOfSync) {
+            this._audio.currentTime = time;
+            dispatch(changeCurrentPercent(player.percent, false)); //turn off outOfSync flag
+        }
+        //handle playing and pausing updates
+        if (!player.isPlaying) {
+            this._audio.pause();
+        } else {
+            this._audio.play();
+        }
+    }
+
+    componentWillUnmount() {
+        this._audio.removeEventListener("ended", this.handleEnded, false);
+        this._audio.removeEventListener("loadedmetadata", this.handleLoadedMetadata, false);
+        this._audio.removeEventListener("loadstart", this.handleLoadStart, false);
+        this._audio.removeEventListener("pause", this.handlePause, false);
+        this._audio.removeEventListener("play", this.handlePlay, false);
+        this._audio.removeEventListener("timeupdate", this.handleTimeUpdate, false);
+        this._audio.removeEventListener("volumechange", this.handleVolumeChange, false);
+    }
+
+    changeSong(changeType) {
+        const { dispatch } = this.props;
+        dispatch(changeSong(changeType));
+    }
+
+    /*
+     * function handleEnded
+     * Handle the event fired when the current song ends.
+     */
+    handleEnded() {
+        if (this.state.repeat) {
+            this._audio.play();
+        } else if (this.state.shuffle) {
+            this.changeSong(CHANGE_TYPES.SHUFFLE);
+        } else {
+            this.changeSong(CHANGE_TYPES.NEXT);
+        }
+    }
+
+    /*
+     * function handleLoadedMetadata
+     * Handle when the audio player has successfully loaded song metadata.
+     */
+    handleLoadedMetadata() {
+        //empty
+    }
+
+    /*
+     * function handleLoadStart
+     * Handle when the player begins to load something.
+     */
+    handleLoadStart() {
+        const { dispatch } = this.props;
+        dispatch(changeCurrentPercent(0.0));
+    }
+
+    handlePause() {
+        // const { dispatch } = this.props;
+        // dispatch(setIsPlaying(false));
+    }
+
+    handlePlay() {
+        // const { dispatch } = this.props;
+        // dispatch(setIsPlaying(true));
+    }
+
+    /*
+     * function handleTimeUpdate
+     * Handle a time update event from the HTMLAudioElement. Don't change time if currently out of sync with the desired time.
+     */
+    handleTimeUpdate(e) {
+        const { dispatch, player, song } = this.props;
+        const currentTime = Math.floor(this._audio.currentTime);
+        if (player.outOfSync) {
+            return;
+        }
+
+        dispatch(changeCurrentPercent(currentTime / (song.duration / 1000.0)));
+    }
+
+    /*
+     * function handleVolumeChange
+     * Handle a volume changed event from the HTMLAudioElement.
+     */
+    handleVolumeChange(e) {
+        const volume = this._audio.volume / VOLUME_SCALE;
+        //LocalStorageUtils.set("volume", volume);
+        this.setState({
+            volume
+        });
+    }
+
+    /*
+     * function onSeekTimeFunc
+     * Handle when the user moves the mouse pointer to seek through the track.
+     * @param   percent     A number between 0.0 and 1.0 indicating seek progress.
+     */
+    onSeekTimeFunc(percent) {
+        const { dispatch, player } = this.props;
+        if (!player.isSeeking) {
+            dispatch(setIsSeeking(true));
+        }
+        dispatch(changeCurrentPercent(percent, true));
+    }
+
+    /*
+     * function onSeekTimeFunc
+     * Handle when the user moves the mouse pointer to change the volume.
+     * @param   percent     A number between 0.0 and 1.0 indicating volume bar percent.
+     */
+    onSeekVolumeFunc(percent) {
+        this.setState({
+            volume: percent
+        });
+        this._audio.volume = percent * VOLUME_SCALE;
+    }
+
+    /*
+     * function onSeekTimeFinishedFunc
+     * Handle when the user finishes seeking through the track.
+     * @param   percent     A number between 0.0 and 1.0 indicating seek progress.
+     */
+    onSeekTimeFinishedFunc(percent) {
+        // const { player } = this.props;
+        const { dispatch } = this.props;
+        dispatch(setIsSeeking(false));
+    }
+
+    /*
+     * function onSeekVolumeFinishedFunc
+     * Handle when the user finishes changing volume.
+     * @param   percent     A number between 0.0 and 1.0 indicating volume percent.
+     */
+    onSeekVolumeFinishedFunc(percent) {
+        this.setState({
+            volume: percent
+        });
+        this._audio.volume = percent * VOLUME_SCALE;
+    }
+
+    toggleMuteFunc() {
+        this._audio.muted = !this._audio.muted;
+        this.setState({
+            muted: this._audio.muted
+        });
+    }
+
+    /*
+     * function togglePlayFunc
+     * Toggle audio playback.
+     */
+    togglePlayFunc() {
+        const { dispatch, player } = this.props;
+        const { isPlaying } = player;
+        if (isPlaying) {
+            //this._audio.pause();
+            dispatch(setIsPlaying(false));
+        } else {
+            //this._audio.play();
+            dispatch(setIsPlaying(true));
+        }
+    }
+
+    /*
+     * function toggleRepeatFunc
+     * Toggle whether to repeat the current track after finishing playback.
+     */
+    toggleRepeatFunc() {
+        this.setState({
+            repeat: !this.state.repeat
+        });
+    }
+
+    /*
+     * function toggleShuffleFunc
+     * Toggle shuffle playback mode.
+     */
+    toggleShuffleFunc() {
+        this.setState({
+            shuffle: !this.state.shuffle
+        });
+    }
+
     render() {
-        const { isMobile, playingSongId } = this.props;
-        if (isMobile) {
-            //TODO: return mobile player
-        }
+        const { dispatch, player, song, users } = this.props;
+        const { muted, volume, repeat, shuffle } = this.state;
+        const user = users[song.user_id];
+        const prevFunc = this.changeSong.bind(this, CHANGE_TYPES.PREV);
+        const nextFunc = this.changeSong.bind(
+            this,
+            this.state.shuffle ? CHANGE_TYPES.SHUFFLE : CHANGE_TYPES.NEXT
+        );
 
-        if (playingSongId == null) {
-            return <div />;
-        }
-
-        return <Player {...this.props} />;
+        return (
+            <Player
+                dispatch={dispatch}
+                muted={muted}
+                nextFunc={nextFunc}
+                onSeekTimeFinishedFunc={this.onSeekTimeFinishedFunc}
+                onSeekVolumeFinishedFunc={this.onSeekVolumeFinishedFunc}
+                onSeekTimeFunc={this.onSeekTimeFunc}
+                onSeekVolumeFunc={this.onSeekVolumeFunc}
+                player={player}
+                prevFunc={prevFunc}
+                repeat={repeat}
+                song={song}
+                shuffle={shuffle}
+                toggleMuteFunc={toggleMuteFunc}
+                togglePlayFunc={togglePlayFunc}
+                toggleRepeatFunc={toggleRepeatFunc}
+                toggleShuffleFunc={toggleShuffleFunc}
+                user={user}
+                volume={volume}
+            />
+        );
     }
 }
 
